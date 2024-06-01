21014892c71f793b511b89913f32726d429aa602
JaeWoongJeong <jjw8959@naver.com>
2024-06-01 12:17:20 +0900

    feat: 파일 불러오기 추가
    파일 불러오기 기능 구현 완료

--- MarkdownEditor/MarkdownEditor/MarkdownEditorApp.swift
+++ MarkdownEditor/MarkdownEditor/MarkdownEditorApp.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct MarkdownEditorApp: App {
     @State private var saveBool = false
     @State private var saveAsBool = false
+    @State private var openBool = false
     @State private var showType = 0 // 0 : EditorOnly , 1 : editor & preview, 2: PreviewOnly
     @State private var document: ReadMeDocument?
     
@@ -35,7 +36,7 @@ struct MarkdownEditorApp: App {
                     document = ReadMeDocument(text: fileVM.textString)
                     saveBool.toggle()
                 }
-//                .keyboardShortcut("s")
+                //                .keyboardShortcut("s")
                 .fileExporter(isPresented: $saveBool,
                               document: document,
                               contentType: .md,
@@ -48,7 +49,25 @@ struct MarkdownEditorApp: App {
                         print(error)
                     }
                 }
+                .keyboardShortcut(KeyEquivalent("s"), modifiers: .command)
                 
+                Button("Open") {
+                    openBool.toggle()
+                }
+                .fileImporter(isPresented: $openBool,
+                              allowedContentTypes: [.plainText])
+                { result in
+                    switch result {
+                    case .success(let file):
+                        guard let url = URL(string: file.absoluteString) else {
+                            print("there's no file")
+                            return
+                        }
+                        loadFileContent(from: url)
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                    }
+                }
                 //                    .fileImporter(isPresented: $saveBool, allowedContentTypes: [.folder]) { result in
                 //                        switch result {
                 //                        case .success(let url) :
@@ -60,20 +79,17 @@ struct MarkdownEditorApp: App {
                 //
                 //                        }
                 //                    }
-                
-                .keyboardShortcut(KeyEquivalent("s"), modifiers: .command)
-                
-                Button("Save As") { saveAsBool.toggle() }
-                    .fileImporter(isPresented: $saveAsBool, allowedContentTypes: [.folder]) { result in
-                        switch result {
-                        case .success(let url) :
-                            print("success: ",url)
-                        case .failure(let error) :
-                            print("fail: ",error)
-                            
-                        }
-                    }
-                    .keyboardShortcut(KeyEquivalent("s"), modifiers: .command.union(.shift))
+                //                Button("Save As") { saveAsBool.toggle() }
+                //                    .fileImporter(isPresented: $saveAsBool, allowedContentTypes: [.folder]) { result in
+                //                        switch result {
+                //                        case .success(let url) :
+                //                            print("success: ",url)
+                //                        case .failure(let error) :
+                //                            print("fail: ",error)
+                //
+                //                        }
+                //                    }
+                //                    .keyboardShortcut(KeyEquivalent("s"), modifiers: .command.union(.shift))
             }
             
             CommandGroup(after: .sidebar) {
@@ -85,7 +101,6 @@ struct MarkdownEditorApp: App {
                     Button("Editor & Preview") {
                         
                     }
-//                    .keyboardShortcut(KeyEquivalent("s"), modifiers: .command)
                     Button("Preview Only") {
                         
                     }
@@ -95,6 +110,20 @@ struct MarkdownEditorApp: App {
             }
         }
         
+    }
+    
+    func loadFileContent(from url: URL) {
+        do {
+            // 파일 URL에서 텍스트 읽기
+            guard url.startAccessingSecurityScopedResource() else { return }
+            let text = try String(contentsOf: url)
+            fileVM.textString = text // 읽은 내용을 상태 변수에 저장
+            print(fileVM.textString)
+            url.stopAccessingSecurityScopedResource()
+        } catch {
+            print("파일 읽기 오류: \(error)")
+            fileVM.textString = "파일을 읽을 수 없습니다."
+        }
     }
     
 }
