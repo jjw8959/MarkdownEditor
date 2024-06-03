4cdf2da31e1add17ffe971495a45a4dd5e0c75b9
JaeWoongJeong <jjw8959@naver.com>
2024-06-03 16:02:37 +0900

    feat: 세그멘트로 뷰 선택
    세그멘트 피커로 세가지(에디터, 프리뷰, 둘다)로 선택해서 보여줄수 있도록 함.
    디폴트는 둘다 보여주기

--- MarkdownEditor/MarkdownEditor/ContentView.swift
+++ MarkdownEditor/MarkdownEditor/ContentView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var fileVM = FileViewModel.shared
-    @State private var document: ReadMeDocument = ReadMeDocument(text: "")
+//    @State private var document: ReadMeDocument = ReadMeDocument(text: "")
+//    @Binding var showType: ShowType
+    @State var showType: ShowType = .editPreview
     
     var body: some View {
-        TextEditor(text: $fileVM.textString)
+        Picker("", selection: $showType) {
+            ForEach(ShowType.allCases, id: \.self) { type in
+                Text(type.rawValue).tag(type)
+            }
+//            Text(ShowType.rawValue).tag(ShowType.editor)
+//            Text("P").tag(ShowType.preview)
+//            Text("blue").tag(ShowType.editPreview)
+        }
+        .pickerStyle(.segmented)
+        .padding()
+        
+        MainContentView(showType: $showType)
+    }
+}
+
+struct MainContentView: View {
+    @ObservedObject var fileVM = FileViewModel.shared
+    
+    @Binding var showType: ShowType
+    
+    var body: some View {
+        switch showType {
+        case .editor:
+            TextEditor(text: $fileVM.textString)
+        case .editPreview:
+            GeometryReader { geometry in
+                HStack {
+                    
+                    TextEditor(text: $fileVM.textString)
+                        .frame(width: geometry.size.width / 2)
+                    ScrollView {
+                        Text(fileVM.textString)
+                            .frame(maxWidth: geometry.size.width / 2, maxHeight: .infinity,alignment: .topLeading)
+                    }
+                }
+            }
+        case .preview:
+            ScrollView {
+                Text(fileVM.textString)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+            }
+        }
     }
 }
 
